ce147bb5c901ed9f76f6d2387544419416f6a071
AntonioTorga <antonio.torga@ug.uchile.cl>
2025-08-26 19:14:34 -0400

    First commit, general structure, not working

---
+++ src/GeoChecker/GeoChecker.py
@@ -0,0 +1,22 @@
+import typer
+from typing_extensions import Annotated
+from pathlib import Path
+from rich import print
+import sys
+import subprocess
+
+app = typer.Typer(name="GeoChecker", help="GeoChecker Command Line Interface", pretty_exceptions_enable=False)
+
+sys.path.append(
+    subprocess.check_output(["grass", "--config", "python_path"], text=True).strip()
+) # add pygrass to path
+
+
+@app.command()
+def check(
+    linkage_file: Annotated[Path, typer.Argument(exists=True, dir_okay=False, file_okay=True, resolve_path=True)],
+    arcs: Annotated[Path, typer.Argument(exists=True, dir_okay=False, file_okay=True, resolve_path=True)],
+    nodes: Annotated[Path, typer.Argument(exists=True, dir_okay=False, file_okay=True, resolve_path=True)],
+    results_folder: Annotated[Path, typer.Option(exists=True, dir_okay=False, file_okay=True, resolve_path=True)] = Path("./").resolve(),
+):
+    print("hello")
