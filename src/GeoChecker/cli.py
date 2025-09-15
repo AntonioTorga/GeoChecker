05e33ccff2f73ca22c52884d5e1dcecb1f93a2f9
AntonioTorga <antonio.torga@ug.uchile.cl>
2025-09-15 09:44:27 -0300

    Added CLI help and /lib to the env

--- src/GeoChecker/cli.py
+++ src/GeoChecker/cli.py
@@ -5,9 +5,14 @@ from rich import print
 import sys
 import subprocess
 
+lib_grass_path = (
+    subprocess.check_output(["grass", "--config", "path"], text=True).strip() + "/lib"
+)
+
 python_grass_path = subprocess.check_output(
     ["grass", "--config", "python_path"], text=True
 ).strip()
+sys.path.append(lib_grass_path)
 sys.path.append(python_grass_path)  # add pygrass to path
 
 from .check.GeoChecker import GeoChecker
@@ -25,31 +30,67 @@ app = typer.Typer(
 def check(
     linkage_file: Annotated[
         Path,
-        typer.Argument(exists=True, dir_okay=False, file_okay=True, resolve_path=True),
+        typer.Argument(
+            exists=True,
+            dir_okay=False,
+            file_okay=True,
+            resolve_path=True,
+            help="Linkage file (.shp) relative or absolute path",
+        ),
     ],
     arcs: Annotated[
         Path,
-        typer.Argument(exists=True, dir_okay=False, file_okay=True, resolve_path=True),
+        typer.Argument(
+            exists=True,
+            dir_okay=False,
+            file_okay=True,
+            resolve_path=True,
+            help="WEAP Arc (.shp) relative or absolute path",
+        ),
     ],
     nodes: Annotated[
         Path,
-        typer.Argument(exists=True, dir_okay=False, file_okay=True, resolve_path=True),
+        typer.Argument(
+            exists=True,
+            dir_okay=False,
+            file_okay=True,
+            resolve_path=True,
+            help="WEAP Node (.shp) relative or absolute path",
+        ),
     ],
     results_folder: Annotated[
         Path,
-        typer.Option(exists=True, dir_okay=True, file_okay=False, resolve_path=True),
+        typer.Option(
+            exists=True,
+            dir_okay=True,
+            file_okay=False,
+            resolve_path=True,
+            help="Folder in which to leave the check results.",
+        ),
     ] = Path("./"),
     catchment_name: Annotated[
         str,
-        typer.Option(),
+        typer.Option(help="Catchment attribute name in the .dbf file"),
     ] = "CATCHMEN",
     groundwater_name: Annotated[
         str,
-        typer.Option(),
+        typer.Option(help="Groundwater attribute name in the .dbf file"),
     ] = "GROUNDWA",
     ds_prefix: Annotated[
         str,
-        typer.Option(),
+        typer.Option(help="Demand site attributes prefix in the .dbf file"),
     ] = "D_",
 ):
-    run(linkage_file, arcs, nodes, results_folder=results_folder, catchment_name=catchment_name, groundwater_name=groundwater_name, ds_prefix=ds_prefix)
+    """GeoChecker runs checks over the linkage file, contrasting it to the WEAP model. It provides
+    reports in .csv, .pdf and .txt format, made to assist the modeler in the correction of the
+    Linkage file.
+    """
+    run(
+        linkage_file,
+        arcs,
+        nodes,
+        results_folder=results_folder,
+        catchment_name=catchment_name,
+        groundwater_name=groundwater_name,
+        ds_prefix=ds_prefix,
+    )
